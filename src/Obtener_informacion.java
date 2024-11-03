54d12e1330e4fa66f78b5c81243014fe8b254068
LuisNunezB <luis.berzunza@outlook.com>
2024-11-02 19:10:27 -0600

    Interacciones
    Clase Interacciones Selenium

--- src/Obtener_informacion.java
+++ src/Obtener_informacion.java
@@ -4,18 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Interacciones {
+public class Obtener_informacion {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.Edge.driver","C:\\Users\\luisb\\Documents\\java_selenium_scripts\\drivers\\edgedriver\\msedgedriver.exe ");
 		WebDriver driver_edge = new EdgeDriver();
 		driver_edge.get("https://www.amazon.com");
-		//send kevys
-		driver_edge.findElement(By.id("twotabsearchtextbox")).sendKeys("Nintendo switch");
-		//crear
-		driver_edge.findElement(By.id("twotabsearchtextbox")).clear();
-		driver_edge.findElement(By.id("nav-hamburger-menu")).click();
-		
+
 	}
 
 }
